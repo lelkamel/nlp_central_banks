aafb151d05768071822ceea487b7ff1519f93f14
SofieneTaamouti <sofienetaamouti@yahoo.fr>
2026-04-08 14:49:46 +0200

    push trust data

--- prep_data_reg.R
+++ prep_data_reg.R
@@ -1,17 +1,17 @@
 library(readxl)
 library(haven)
 library(tidyverse)
-
+library(writexl)
 
 #US Fed
-confidence_Fed = read_excel("data/fed_confidence.xlsx")
+confidence_Fed = read_excel("data/fed_confidence.xlsx") %>%
+  arrange(Year)
 
 #Bank of England
 confidence_BoE <- read_excel("data/BoE_confidence.xlsx", 
 range = "A238:DC248",
 col_names = FALSE) %>%
   na.omit()
-
 
 # renommer la première colonne
 names(confidence_BoE)[1] <- "category"
@@ -32,17 +32,39 @@ confidence_BoE_tidy <- confidence_BoE %>%
     values_to = "Total satisfied"
   ) %>%
   filter(category == "Total satisfied") %>%
-  select(-category)
+  select(-category) %>%
+  mutate(year = year(month)) %>%
+  group_by(year) %>%
+  summarise("Total satisfied" = mean(`Total satisfied`, na.rm = TRUE))
 
 #ECB
 #variable of interest: treu_ecb. 1: trust ; 2: no trust
 
-ecb_confidence <- read_dta("data/harmonised_EB_2004-2021_v3-0-0.dta") 
+df_ecb <- read_dta("data/harmonised_EB_2004-2021_v3-0-0.dta") 
 
-ecb_confidence <- ecb_confidence %>%
+df_ecb_p2 <- read_excel("data/ecb_complement.xlsx") %>%
+  mutate(year = year(year)) %>%
+  group_by(year) %>%
+  summarise(prop_confident = mean(prop_confident))
+
+ecb_confidence_p1 <- df_ecb %>%
   select(year, treu_ecb) %>%
-  na.omit() 
+  na.omit() %>%
+  group_by(year) %>%
+  summarise(prop_confident = mean(treu_ecb == 1)*100)
 
+ecb_confidence <- bind_rows(df_ecb_p2, ecb_confidence_p1) %>%
+  arrange(year)
 
+#confidence dataset
+names(ecb_confidence)[1] <- "date"
+names(confidence_BoE_tidy)[1] <- "date"
+names(confidence_Fed)[1] <- "date"
 
+cf_df <- ecb_confidence %>%
+  inner_join(confidence_BoE_tidy, by = "date") %>%
+  inner_join(confidence_Fed, by = "date")
 
+names(cf_df)[2:4] <- c("ECB_trust", "BoE_trust", "Fed_trust")
+
+write_xlsx(cf_df, "data/bc_trust_data.xlsx")
